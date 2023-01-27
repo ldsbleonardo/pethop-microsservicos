a2778c30572665c46754fb1bb59c125b1a1b747f
ldsbleonardo <ldsbleonardo@gmail.com>
2023-01-27 16:11:05 -0300

    Add files via upload

---
+++ configserver/src/test/java/br/infnet/leandro/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package br.infnet.leandro.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
